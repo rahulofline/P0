6d620e2d60fd9362f73329c393a9e440e2c9528a
rahulofline <rahulrajput844088@gmail.com>
2025-07-13 08:50:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"SERVER RUNNING => TRICKS BY  JUSTIN SIR ")
+        self.wfile.write(b"SERVER RUNNING => TRICKS BY  AN1L SIR ")
 
 def execute_server():
     PORT = 4000
