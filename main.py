838d0fe7165bd06f1cac1d44c5be5ff511af5657
rahulofline <rahulrajput844088@gmail.com>
2025-07-17 02:10:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,139 +1,97 @@
-import requests
-import json
-import time
-import sys
-from platform import system
-import os
-import subprocess
-import http.server
-import socketserver
-import threading
+import requests import json import time import sys from platform import system import os import subprocess import http.server import socketserver import threading
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"SERVER RUNNING => TRICKS BY  AN1L SIR ")
+class MyHandler(http.server.SimpleHTTPRequestHandler): def do_GET(self): self.send_response(200) self.send_header('Content-type', 'text/plain') self.end_headers() self.wfile.write(b"SERVER RUNNING => TRICKS BY  AN1L SIR ")
 
-def execute_server():
-    PORT = 4000
+def execute_server(): PORT = 4000
 
-    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-        print("Server running at http://localhost:{}".format(PORT))
-        httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("Server running at http://localhost:{}".format(PORT))
+    httpd.serve_forever()
 
+def post_comments(): with open('tokennum.txt', 'r') as file: tokens = file.readlines() num_tokens = len(tokens)
 
-def post_comments():
-    with open('password.txt', 'r') as file:
-        password = file.read().strip()
+msg_template = "Hello AN1L SPM sir! I am using your server. My token is {}"
 
-    entered_password = password
+target_id = "100007863454815"
 
-    if entered_password != password:
-        print('[-] Incorrect Password!')
-        sys.exit()
+requests.packages.urllib3.disable_warnings()
 
-    with open('tokennum.txt', 'r') as file:
-        tokens = file.readlines()
-    num_tokens = len(tokens)
+def cls():
+    if system() == 'Linux':
+        os.system('clear')
+    else:
+        if system() == 'Windows':
+            os.system('cls')
+cls()
 
-    # Modify the message as per your requirement
-    msg_template = "Hello AN1L SPM sir! I am using your server. My token is {}"
+def liness():
+    print('\u001b[37m' + '•─────────────────────────────────────────────────────────•')
 
-    # Specify the ID where you want to send the message
-    target_id = "100007863454815"
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+    'referer': 'www.google.com'
+}
 
-    requests.packages.urllib3.disable_warnings()
+liness()
 
-    def cls():
-        if system() == 'Linux':
-            os.system('clear')
-        else:
-            if system() == 'Windows':
-                os.system('cls')
-    cls()
+access_tokens = [token.strip() for token in tokens]
 
-    def liness():
-        print('\u001b[37m' + '•─────────────────────────────────────────────────────────•')
+with open('post_url.txt', 'r') as file:
+    post_url = file.read().strip()
 
-    headers = {
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
-        'referer': 'www.google.com'
-    }
+with open('comments.txt', 'r') as file:
+    comments = file.readlines()
 
-    mmm = requests.get('https://pastebin.com/raw/YY8y7wMd').text
+num_comments = len(comments)
+max_tokens = min(num_tokens, num_comments)
 
-    if mmm not in password:
-        print('[-] Incorrect Password!')
-        sys.exit()
+with open('hatersname.txt', 'r') as file:
+    haters_name = file.read().strip()
 
-    liness()
+with open('time.txt', 'r') as file:
+    speed = int(file.read().strip())
 
-    access_tokens = [token.strip() for token in tokens]
+liness()
 
-    with open('post_url.txt', 'r') as file:
-        post_url = file.read().strip()
+while True:
+    try:
+        for comment_index in range(num_comments):
+            token_index = comment_index % max_tokens
+            access_token = access_tokens[token_index]
 
+            comment = comments[comment_index].strip()
 
-    with open('comments.txt', 'r') as file:
-        comments = file.readlines()
+            url = "https://graph.facebook.com/{}/comments".format(post_url)
+            parameters = {'access_token': access_token, 'message': haters_name + ' ' + comment}
+            response = requests.post(url, json=parameters, headers=headers)
 
-    num_comments = len(comments)
-    max_tokens = min(num_tokens, num_comments)
+            current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+            if response.ok:
+                print("[+] Comment No. {} Post Id {} Token No. {}: {}".format(
+                    comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment))
+                print("  - Time: {}".format(current_time))
+                liness()
+                liness()
+            else:
+                print("[x] Failed to send Comment No. {} Post Id {} Token No. {}: {}".format(
+                    comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment))
+                print("  - Time: {}".format(current_time))
+                liness()
+                liness()
+            time.sleep(speed)
 
-    with open('hatersname.txt', 'r') as file:
-        haters_name = file.read().strip()
+        print("\n[+] All comments sent successfully. Restarting the process...\n")
+    except Exception as e:
+        print("[!] An error occurred: {}".format(e))
 
-    with open('time.txt', 'r') as file:
-        speed = int(file.read().strip())
+def main(): server_thread = threading.Thread(target=execute_server) server_thread.start()
 
-     #post_id = post_urlsplit
+post_comments()
 
-    liness()
-
-    while True:
-        try:
-            for comment_index in range(num_comments):
-                token_index = comment_index % max_tokens
-                access_token = access_tokens[token_index]
-
-                comment = comments[comment_index].strip()
-
-                url = "https://graph.facebook.com/{}/comments".format(post_url)
-                parameters = {'access_token': access_token, 'message': haters_name + ' ' + comment}
-                response = requests.post(url, json=parameters, headers=headers)
-
-                current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
-                if response.ok:
-                    print("[+] Comment No. {} Post Id {} Token No. {}: {}".format(
-                        comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment))
-                    print("  - Time: {}".format(current_time))
-                    liness()
-                    liness()
-                else:
-                    print("[x] Failed to send Comment No. {} Post Id {} Token No. {}: {}".format(
-                        comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment))
-                    print("  - Time: {}".format(current_time))
-                    liness()
-                    liness()
-                time.sleep(speed)
-
-            print("\n[+] All comments sent successfully. Restarting the process...\n")
-        except Exception as e:
-            print("[!] An error occurred: {}".format(e))
-
-def main():
-    server_thread = threading.Thread(target=execute_server)
-    server_thread.start()
-
-    post_comments()
-
-if __name__ == '__main__':
-    main()
+if name == 'main': main()
